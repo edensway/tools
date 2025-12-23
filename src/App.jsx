db531e05ebb05ea1826e9298750d56dead0efe27
AdyGreg <adygreg@adygregs-macbook.local>
2025-12-23 17:58:29 +0530

    added to homescreen option

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import "./styles/app.css";
 import Navigation from "./components/Navigation";
 
@@ -9,6 +11,33 @@ import BMR from "./pages/BMR";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstall, setShowInstall] = useState(false);
+
+  useEffect(() => {
+    const handler = (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setShowInstall(true);
+    };
+
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () =>
+      window.removeEventListener("beforeinstallprompt", handler);
+  }, []);
+
+  const handleInstall = async () => {
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+    await deferredPrompt.userChoice;
+
+    setDeferredPrompt(null);
+    setShowInstall(false);
+  };
+
   return (
     <div className='main-container'>
       <Navigation />
@@ -19,6 +48,14 @@ function App() {
         <Route path="/bmr" element={<BMR />} />
         <Route path="/body-comp" element={<BodyComposition />} />
       </Routes>
+
+      {showInstall && (
+        <div className="install-popup">
+          <button className="button body" onClick={handleInstall}>Add to Home Screen</button>
+          <button className="button body btn-unactive" onClick={() => setShowInstall(false)}> Not Now </button>
+        </div>
+      )}
+
     </div>
   );
 }
